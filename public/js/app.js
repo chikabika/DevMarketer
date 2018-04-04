2e42e70e5758d7d3390a8250a86e9e2473bb7604
halaseddine <salah.fkinich@finacards.ma>
2018-04-04 11:24:34 +0000

    add nav bar style 8

--- public/js/app.js
+++ public/js/app.js
@@ -984,6 +984,20 @@ window.Vue = __webpack_require__(34);
 Vue.use(__WEBPACK_IMPORTED_MODULE_0_buefy___default.a);
 // Vue.component('example', require('./components/Example.vue'));
 
+var app = new Vue({
+    el: '#app',
+    data: {}
+
+});
+
+$(document).ready(function () {
+
+    $('button.dropdown').hover(function (e) {
+
+        $(this).toggleClass('is-open');
+    });
+});
+
 /***/ }),
 /* 11 */
 /***/ (function(module, exports, __webpack_require__) {
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,3 +8,20 @@ import Buefy from 'buefy';
 Vue.use(Buefy);
 // Vue.component('example', require('./components/Example.vue'));
 
+var app = new Vue({
+el:'#app',
+data:{}
+
+});
+
+
+
+$(document).ready(function(){
+
+    $('button.dropdown').hover(function(e){
+
+  $(this).toggleClass('is-open');
+    });
+
+
+});
